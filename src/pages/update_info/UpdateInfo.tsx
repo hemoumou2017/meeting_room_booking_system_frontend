91a7fef72b4eccc474bcf6d7e75acccce0b24358
何欣 <1254409474@qq.com>
2024-10-16 14:47:07 +0800

    优化

--- src/pages/update_info/UpdateInfo.tsx
+++ src/pages/update_info/UpdateInfo.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2024-10-08 15:24:22
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2024-10-15 16:44:38
+ * @LastEditors: 何欣 1254409474@qq.com
+ * @LastEditTime: 2024-10-16 14:45:45
  * @FilePath: /meeting_room_booking_system_frontend/src/pages/update_info/UpdateInfo.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -49,7 +49,7 @@ export function UpdateInfo() {
                 message.error(msg);
             }
         } else {
-            message.error('系统繁忙，请稍后再试');
+            message.error(res.data?.data || '系统繁忙，请稍后再试');
         }
     }, []);
 
